2a2d7eff8b0fbdbc4ac76283c29759c8e33e42b2
Shoubhik <shoubhik2396@gmail.com>
2021-06-15 00:01:37 +0530

    remvoed console

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -2,7 +2,6 @@ import React from "react";
 import Pagination from "@material-ui/lab/Pagination";
 
 function PaginationComponent(props) {
-  console.log(props);
   return (
     <Pagination
       count={props.total}
